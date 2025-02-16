3fc72196292fd668741a52038ff81b5c134b35e3
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2025-02-16 11:30:11 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,6 +1,6 @@
 # config.py
-api_id = ""
-api_hash = ""
-bot_token = ""
-MONGO_URL = ""
+api_id = "20533795"
+api_hash = "f6cadf28523943f525e706e6ace8a250"
+bot_token = "6639816047:AAFGJYutAwQm-PBOLrhJkNjpw8znFovMocQ"
+MONGO_URL = "mongodb+srv://ioiioikamer244:0ujOtQINl0ZnOxIr@cluster0.fpx5z.mongodb.net/?retryWrites=true&w=majority"
 OWNER_ID = 5630057244
